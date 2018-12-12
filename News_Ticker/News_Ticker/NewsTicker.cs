bb6d34bf159f391088183dcdfdadaec6a21ea863
GOULOMpro <christopher.wendholt.2000@gmail.com>
2018-12-12 02:24:28 +0100

    TEST

--- News_Ticker/News_Ticker/NewsTicker.cs
+++ News_Ticker/News_Ticker/NewsTicker.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace News_Ticker
 {
-	public partial class Form1 : Form
+	public partial class NewsTicker : Form
 	{
-		public Form1()
+		public NewsTicker()
 		{
 			InitializeComponent();
 		}
